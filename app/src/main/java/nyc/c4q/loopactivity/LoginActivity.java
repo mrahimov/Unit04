98f1489992492800360836aafb6e772dc98f24e7
Murad Rahimov <rahimov.mu@gmail.com>
2017-12-06 20:37:22 -0500

    added part02

--- app/src/main/java/nyc/c4q/loopactivity/LoginActivity.java
+++ app/src/main/java/nyc/c4q/loopactivity/LoginActivity.java
@@ -1,8 +1,58 @@
 package nyc.c4q.loopactivity;
 
-/**
- * Created by muridjonrahimov on 12/6/17.
- */
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    static final String PREF_KEY = "shared";
+    EditText emailEditText;
+    EditText passwordEditText;
+    CheckBox checkBox;
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        checkBox = findViewById(R.id.checkBox);
+        button = findViewById(R.id.login_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (emailEditText != null && passwordEditText != null) {
+                    if (emailEditText.getText().toString().equals("user@aol.com") &&
+                            passwordEditText.getText().toString().equals("password1234")) {
+                        Intent intent = new Intent(LoginActivity.this, ListActivity.class);
+                        intent.putExtra("email", emailEditText.getText().toString());
+                        intent.putExtra("password", passwordEditText.getText().toString());
+                        startActivity(intent);
+
+                    }
+                }
+                if(checkBox.isChecked()) {
+
+                    String email = emailEditText.getText().toString();
+                    String password = passwordEditText.getText().toString();
+
+                    SharedPreferences.Editor editor = getSharedPreferences(PREF_KEY, MODE_PRIVATE).edit();
+                    editor.putString(email, "No email defined");
+                    editor.putString(password, "No password defined");
+                    editor.apply();
+                }
+
+            }
+        });
+    }
 }
